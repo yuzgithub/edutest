2f681e5be561f4aebd27497b4042ea9d65d08932
jiangyunwei1992 <jiang.yun@husky.neu.edu>
2016-08-26 12:47:50 -0400

    the fake data in server pass

--- server/app.js
+++ server/app.js
@@ -1,21 +1,20 @@
 module.exports = function(app) {
 
-    var models = require("./models/models.server")();
-    require("./services/user.service.server.js")(app, models);
-    require("./services/website.service.server.js")(app, models);
-    require("./services/page.service.server.js")(app, models);
-    require("./services/widget.service.server.js")(app, models);
-    require("./services/movie.service.server.js")(app, models);
+    // var models = require("./models/models.server")();
+    require("./services/movie.service.server.js")(app);
+    // require("./services/website.service.server.js")(app, models);
+    // require("./services/page.service.server.js")(app, models);
 
 
-    app.get("/users/:id", function(req,res){
-        var id = req.params.id;
-        for(var i in users){
-            if(users[i]._id === id){
-                res.send(users[i]);
-                return;
-            }
-        }
-        res.send({});
-    });
+    //
+    // app.get("/users/:id", function(req,res){
+    //     var id = req.params.id;
+    //     for(var i in users){
+    //         if(users[i]._id === id){
+    //             res.send(users[i]);
+    //             return;
+    //         }
+    //     }
+    //     res.send({});
+    // });
 };
--- server/modelssss/models.server.js
+++ server/modelssss/models.server.js
@@ -4,8 +4,8 @@ module.exports = function()
         userModel:require("./user/user.model.server")(),
         websiteModel: require("./website/website.model.server")(),
         pageModel: require("./page/page.model.server")(),
-        widgetModel: require("./widgets/widget.model.server")(),
-        movieModel: require("./movie/movie.model.server.js")()
+        // widgetModel: require("./widgets/widget.model.server")(),
+        // movieModel: require("./movie/movie.model.server.js")()
 
 
     }
